ebce5500d11a0414e45244558daf37428e64b110
PantheraDigital <pantheradigitalonline@gmail.com>
2023-04-16 19:08:10 -0700

    QR button added

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 var linkWindow = document.getElementById("links-window");
 var projectWindow = document.getElementById("projects-window");
 var aboutWindow = document.getElementById("about-window");
+var qrOverlay = document.getElementById("qr-overlay");
+var qrOverlayButton = document.getElementById("qr-button");
 
 document.getElementById("link-button").addEventListener("click", function(){ShowHiddenWindow("links-window")});
 document.getElementById("projects-button").addEventListener("click", function(){ShowHiddenWindow("projects-window")});
 document.getElementById("about-button").addEventListener("click", function(){ShowHiddenWindow("about-window")});
+
+document.getElementById("qr-button").addEventListener("click", function(){
+    if(this.style.backgroundImage == "none"){
+        this.style.backgroundImage = "url('././ProjectPics/QR.png')";
+        this.style.color = "rgba(255, 255, 255, 0)";
+        qrOverlay.style.display = "none";
+    }
+    else{
+        this.style.backgroundImage = "none";
+        this.style.color = "black";
+        qrOverlay.style.display = "block";
+    }
+});
 
 var closeWindowButtons = document.getElementsByClassName("close-hidden-window-button");
 for(let i = 0; i < closeWindowButtons.length; i++){
@@ -14,26 +29,14 @@ for(let i = 0; i < closeWindowButtons.length; i++){
 function HideHiddenWindows(event){
     let target = event.target;
     target.parentElement.style.display = "none";
-}
 
-function ShowLinkWindow(){
-    if(linkWindow){
-        linkWindow.style.display = "block";
-    }
-}
-function ShowProjectWindow(){
-    if(projectWindow){
-        projectWindow.style.display = "block";
-    }
-}
-function ShowAboutWindow(){
-    if(aboutWindow){
-        aboutWindow.style.display = "block";
-    }
+    qrOverlayButton.style.display = "block";
 }
 
 function ShowHiddenWindow(windowID){
     let window = document.getElementById(windowID);
     window.style.display = "block";
+
+    qrOverlayButton.style.display = "none";
 }
 
